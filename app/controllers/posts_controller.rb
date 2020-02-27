8deab4b2a0f034467cbf9ca16637534fd57a23af
Timothy Bransford <bransfordware@gmail.com>
2020-02-27 01:15:08 -0500

    Implement creation process for posts and refactor

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,35 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
+
   def index
 
+  end
+
+  def new
+    @post = Post.new 
+  end
+
+  def create
+    @post = Post.new(post_params)
+    
+    if @post.save
+      redirect_to @post
+    else
+      render new_post_path
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:date, :rationale)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -12,4 +12,22 @@ context 'navigate' do
       expect(page).to have_content(/Posts/)
     end
   end
+
+  describe "creation" do
+    before do
+      visit new_post_path
+    end
+    it "has a new form that can be reached" do
+      expect(page.status_code).to eq(200)
+    end
+
+    it "can be createed from new form page" do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "A rationale"
+
+      click_on "Save"
+      expect(page).to have_content("A rationale")
+    end
+  end
+
 end
