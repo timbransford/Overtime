1bcca297216b9b1cf4d81d7875b629dcc1b5a4a6
Timothy Bransford <bransfordware@gmail.com>
2020-02-27 00:03:36 -0500

    Used resource generator for post and created validations

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe Post, type: :model do
+  context "Creation" do
+    before do
+      @post = Post.create(date: Date.today, rationale: "Anything")
+    end
+    it "can be created" do
+      expect(@post).to be_valid
+    end
+
+    it "cannot be created without a date" do
+      @post.date = nil
+      expect(@post).to_not be_valid
+    end
+
+    it "cannot be created without a rationale" do
+      @post.rationale = nil
+      expect(@post).to_not be_valid
+    end
+  end
+end
